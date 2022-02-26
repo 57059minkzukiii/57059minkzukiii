c98135afe41024889ce6e42590e8767a050358ab
57059minkzukiii <98304382+57059minkzukiii@users.noreply.github.com>
2022-02-26 22:59:47 +0700

    Create Font.php

---
+++ Font.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("MiPancake.ttf");
+      }
+      @font-face{
+        font-family: "MyFont2";
+        src: url("Fcflexica.ttf");
+      }
+      #font1{
+        font-family: "MyFont1";
+      }
+      #font2{
+        font-family:  "MyFont2";
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="font1">นางสาวถุงเงิน ทองงาม เลขที่ 17 ห้อง ม.4/12 เลขประจำตัว 57059</h1>
+    <h1 id="font2">นางสาวถุงเงิน ทองงาม เลขที่ 17 ห้อง ม.4/12 เลขประจำตัว 57059</h1>
+  </body>
+</html>
