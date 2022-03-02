7d38cc4bed521c44641b60e400d3103b3a50b54e
57059minkzukiii <98304382+57059minkzukiii@users.noreply.github.com>
2022-03-02 22:49:16 +0700

    Create Table.php

---
+++ Table.php
@@ -0,0 +1,45 @@
+<html>
+  <head>
+    <title>การสร้างตาราง</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>Twitter/Instragram</th>
+      </tr>
+      <tr>
+        <td>1</td>                  
+        <td>Kentob yamazaki</td>
+        <td><img src="https://uncommontrax.com/wp-content/uploads/2021/01/ebf8521eb1427dcf6c309e6b4b0a47ee.png" width="45px"></td>
+        <td><a href="https://instagram.com/o">Instragram</a></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Chaeunwoo</td>
+        <td><img src="https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2021/12/12/2536920885.jpg" width="45px"></td>
+        <td><a href="https://instagram.com/nozeworld?utm_medium=copy_link">Instragram</a></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Thee</td>
+        <td><img src="http://www.news.in.th/img_1/2/211211d5nwg.jpg" width="45px"></td>
+        <td><a href="https://instagram.com/thee.phee?utm_medium=copy_link">Instragram</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>Phee</td>
+        <td><img src="https://entertain.teenee.com/thaistarphoto/img8/348350.jpg" width="45px"></td>
+        <td><a href="https://instagram.com/thee.phee?utm_medium=copy_link">Instragram</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>sunghoon</td>
+        <td><img src="https://kprofiles.com/wp-content/uploads/2020/08/Sunghoon-2-990x1290.Jose" width="45px"></td>
+        <td><a href="https://instagram.com/enhypen?utm_medium=copy_link">Instragram</a></td>
+      </tr>
+    </table>
+  </body>
+</html>
